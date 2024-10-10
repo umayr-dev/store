6add85848f68e80e7f15c54f29a9bffbc9a4fbea
Umar <abuqodir0107@gmail.com>
2024-10-10 11:52:48 +0500

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='footer'>
+        <footer>
             <div className="container">
-                Footer
+                <div className="footer">
+                    <div className="footer-top">
+                        <div className="column">
+                            <h2>Biz Haqimizda</h2>
+                            <p>Topshirish punktlari</p>
+                            <p>Vakansiyalar</p>
+                        </div>
+                        <div className="column">
+                            <h2>Foydalanuvchilarga</h2>
+                            <p>Biz bilan bogʻlanish</p>
+                            <p>Savol-Javob</p>
+                        </div>
+                        <div className="column">
+                            <h2>Tadbirkorlarga</h2>
+                            <p>Uzumda soting</p>
+                            <p>Sotuvchi Kabineti</p>
+                        </div>
+                        <div className="column">
+                            <h2>Ilovani yuklab olish</h2> 
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     )
